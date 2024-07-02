a2e3faddaac62a7e03366603cdbd6c665255c47f
KreenaShah <102236893+KreenaShah@users.noreply.github.com>
2024-07-02 12:28:40 +0530

    implemented fuzzy search

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Main from './components/Main';
-import FileUpload from './components/FileUpload';
 import FileList from './components/FileList';
 
 const App = () => {
   return (
     <Router>
         <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/upload" element={<FileUpload />} />
-          <Route path="/files" element={<FileList />} />
+          <Route path="/" element={<FileList />} />
         </Routes>
     </Router>
   );
--- client/src/components/FileList.js
+++ client/src/components/FileList.js
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import TinyMCEEditor from "./TinyMCEEditor";
+import Fuse from "fuse.js";
 
 const FileList = () => {
+  const [file, setFile] = useState(null); // uploading file
   const [files, setFiles] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
   const [selectedFileId, setSelectedFileId] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     const fetchFiles = async () => {
       try {
         const res = await axios.get("http://localhost:5000/all");
         setFiles(res.data);
+        setSearchResults(res.data);
       } catch (error) {
         console.error("Error fetching files:", error);
       }
@@ -19,8 +24,21 @@ const FileList = () => {
     fetchFiles();
   }, []);
 
+  const handleSearch = () => {
+    if (searchTerm.trim() === "") {
+      setSearchResults(files);
+      return;
+    }
+
+    const fuse = new Fuse(files, {
+      keys: ["originalName"],
+      threshold: 0.7, // value closer to 0 = stricter matching (more precise results) && closer to 1 allows for more lenient matching (broader results).
+    });
+    const results = fuse.search(searchTerm).map((result) => result.item);
+    setSearchResults(results);
+  };
+
   const handleViewFile = (fileId) => {
-    console.log("View file:", fileId);
     window.open(`http://localhost:5000/files/${fileId}`, "_blank");
   };
 
@@ -30,22 +48,55 @@ const FileList = () => {
 
   const handleDeleteFile = async (fileId) => {
     try {
-      console.log("handleDeleteFile")
       await axios.delete(`http://localhost:5000/${fileId}`);
       setFiles(files.filter(file => file._id !== fileId));
+      setSearchResults(searchResults.filter((file) => file._id !== fileId));
       console.log("File deleted successfully");
     } catch (error) {
       console.error("Error deleting file:", error);
     }
   };
 
+  const onFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const onFileUpload = async () => {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      const res = await axios.post('http://localhost:5000/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      console.log(res.data);
+      setFiles((prevFiles) => [...prevFiles, res.data]);
+      setSearchResults((prevFiles) => [...prevFiles, res.data]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div>
-      <h2>Files</h2>
+      <h2>File Structure</h2>
+      <input type="file" onChange={onFileChange} />
+      <button onClick={onFileUpload}>Upload</button>
+      <hr></hr>
+      <input
+        type="text"
+        placeholder="Search files..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+      <hr />
       <table>
         <thead>
           <tr>
-            <th>Number</th>
+            <th>No</th>
             <th>File Name</th>
             <th>View</th>
             <th>Edit</th>
@@ -53,7 +104,7 @@ const FileList = () => {
           </tr>
         </thead>
         <tbody>
-          {files.map((file, index) => (
+          {searchResults.map((file, index) => (
             <tr key={file._id}>
               <td>{index + 1}</td>
               <td>
